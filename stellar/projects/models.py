165959208881c6b3000d98109a4e41102cd20776
gurunadig <rugvedhi@gmail.com>
2022-01-09 16:06:29 +0530

    till category filter

--- stellar/projects/models.py
+++ stellar/projects/models.py
@@ -3,12 +3,12 @@ from django.db import models
 class Product(models.Model):
     product_name = models.CharField(max_length=200, blank=False, null=False)
     image = models.ImageField(default="400-200.png", null=False, blank=True)
-    product_category = models.ForeignKey('Product_category', null=True, blank=True, on_delete=models.SET_NULL)
+    product_category = models.ForeignKey('Product_category', null=False, blank=False, on_delete=models.CASCADE)
     product_description = models.TextField(blank=False, null=False)
     product_technical_specs = models.TextField(blank=False, null=False)
     price = models.IntegerField(blank=False, null=False)
     product_tag = models.CharField(max_length=50, blank=False, null=False)
-    category = models.ForeignKey('Category', null=True, blank=True, default=1, on_delete=models.SET_NULL)
+    category = models.ForeignKey('Category', null=False, blank=False, default=1, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.product_name
